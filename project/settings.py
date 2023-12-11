05ee637ce6df2096ab6f78a8b79df0460473abd9
AkylaiTopoeva <akylaytop@gmail.com>
2023-12-11 19:47:13 +0600

    add templates, change in settings, add html files for login and signup

--- project/settings.py
+++ project/settings.py
@@ -60,7 +60,7 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "home" 
+LOGOUT_REDIRECT_URL = "home"
