204ae348918bd9bb6cf5b46ced2c65e4119f845a
AkylaiTopoeva <akylaytop@gmail.com>
2023-12-11 19:40:31 +0600

    admin panel

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class CustomUser(models.AbstractUser):
+class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
