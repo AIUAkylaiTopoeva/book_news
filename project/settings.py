dad8d79686223136647d4f2b46517e7fdb4c9300
AkylaiTopoeva <akylaytop@gmail.com>
2023-12-11 20:49:10 +0600

    try to add icon , but it dont work

--- project/settings.py
+++ project/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd Party
+    "crispy_forms", 
+    "crispy_bootstrap5",
+
     #App
     'accounts',
     'pages',
-    
+
 
 ]
 
@@ -129,5 +133,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 LOGIN_REDIRECT_URL = "home" 
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" 
+CRISPY_TEMPLATE_PACK = "bootstrap5"
