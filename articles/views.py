d1a9aaea57a44203e5312e77eed7ffb3aac311e0
AkylaiTopoeva <akylaytop@gmail.com>
2023-12-20 13:33:06 +0600

    install bootstrap and change template

--- articles/views.py
+++ articles/views.py
@@ -52,7 +52,7 @@ class ArticleCreateView(LoginRequiredMixin, CreateView):
 
 class CommentGet(DetailView): 
     model = Article
-    template_name = "article_detail.html"
+    template_name = "article/article_detail.html"
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs) 
@@ -62,7 +62,7 @@ class CommentGet(DetailView):
 class CommentPost(SingleObjectMixin, FormView): 
     model = Article
     form_class = CommentForm 
-    template_name = "article_detail.html"
+    template_name = "article/article_detail.html"
 
     def post(self, request, *args, **kwargs): 
         self.object = self.get_object()
@@ -82,6 +82,7 @@ class ArticleDetailView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         view = CommentGet.as_view()
         return view(request, *args, **kwargs)
+    
     def post(self, request, *args, **kwargs): 
         view = CommentPost.as_view()
         return view(request, *args, **kwargs)
