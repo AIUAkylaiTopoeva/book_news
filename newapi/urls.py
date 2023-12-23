f2dd245c2058e3e13d2965d72df923a2455dec87
AkylaiTopoeva <akylaytop@gmail.com>
2023-12-23 19:39:50 +0600

    add listapi view

--- newapi/urls.py
+++ newapi/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
 
-from .views import NewList, NewDetail
+from .views import NewList, NewDetail, NewCreate
 
 urlpatterns = [
     path("<int:pk>/", NewDetail.as_view(), name="new_detail"), 
     path("", NewList.as_view(), name="new_list"),
+    path('create/', NewCreate.as_view(), name= 'new_create')
 ]
--- newapi/views.py
+++ newapi/views.py
@@ -3,10 +3,14 @@ from rest_framework import generics
 from .models import New
 from .serializers import NewSerializers
 
-class NewList(generics.ListCreateAPIView): 
+class NewCreate(generics.ListCreateAPIView): 
     queryset = New.objects.all() 
     serializer_class = NewSerializers
 
 class NewDetail(generics.RetrieveUpdateDestroyAPIView): 
     queryset = New.objects.all()
+    serializer_class = NewSerializers
+
+class NewList(generics.ListAPIView): 
+    queryset = New.objects.all() 
     serializer_class = NewSerializers
